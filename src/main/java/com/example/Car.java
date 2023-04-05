6a120ccd161ba76894775ffd2b15625765d6f117
Luca Strano <stranoluca469955@gmail.com>
2023-04-05 08:47:07 +0200

    feat(Car): added example class on which to do static analysis of the code

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class Car{
+    private String color;
+    private int speed;
+    private boolean on;
+
+    public Car(String c){
+        color = c;
+        speed = 0;
+        on = false;
+    }
+    
+    public void turnOn(){ on = true; }
+    public void turnOff(){ on = false; }
+    public void accelerate(){
+        System.out.println("Sto aumentando la velocità");
+        if(on){
+            speed++;
+            System.out.println("Velocità aumentata di 1");
+        }
+    }
+    public String getColor(){ return color; }
+    public int getSpeed(){ return speed; }
+    public boolean isOn(){ return on; }
+    public void printCarInfo(){
+        System.out.println("Car color: " + getColor());
+        System.out.println("Car speed: " + getSpeed());
+    }
+}
