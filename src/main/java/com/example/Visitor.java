26a8d213b26bb7ed05f22a9ea210056067f802b7
Luca Strano <stranoluca469955@gmail.com>
2023-04-12 12:47:19 +0200

    refactor(Visitor.java): eliminated useless methods and semplified MethodBlockStmt class

--- src/main/java/com/example/Visitor.java
+++ src/main/java/com/example/Visitor.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.ast.visitor.VoidVisitor;
 import com.github.javaparser.ast.stmt.BlockStmt;
@@ -18,61 +17,27 @@ public class Visitor{
     public static void main(String[] args) throws Exception{
         
         CompilationUnit cu = StaticJavaParser.parse(new FileInputStream(FILE_PATH));
+
+        List<BlockStmt> methodBlocks = new ArrayList<>();
+        VoidVisitor<List<BlockStmt>> methodBlockStmt = new MethodBlockStmt();
+        methodBlockStmt.visit(cu, methodBlocks);
+
+        //System.out.println(methodBlocks.get(1).getStatements().get(0));
+
+        for(int i=0;i<methodBlocks.size();i++){ //for each block i ...
+            for(int j=0;j<methodBlocks.get(i).getStatements().size();j++){ //... for each statement j of block i
+                System.out.println(methodBlocks.get(i).getStatements().get(j));
+            }
+            System.out.println("\n\n");
+        }
         
-        System.out.println("\t***FIRST VISITOR***");
-        //For First Visitor
-        VoidVisitor<Void> methodNameVisitor = new MethodNamePrinter();
-        methodNameVisitor.visit(cu, null);
-
-        System.out.println("\n\n\t***SECOND VISITOR***");
-        //For Second Visitor
-        List<String> methodNames = new ArrayList<>();
-        VoidVisitor<List<String>> methodNameCollector = new MethodNameCollector();
-        methodNameCollector.visit(cu, methodNames);
-        methodNames.forEach(n -> System.out.println("Method name collected: " + n));
-
-        System.out.println("\n\n\t***THIRD VISITOR***");
-        //For Third Visitor
-        List<BlockStmt> methodStatements = new ArrayList<>();
-        VoidVisitor<List<BlockStmt>> methodStatementsCollector= new MethodStatementsCollector();
-        methodStatementsCollector.visit(cu, methodStatements);
-            //Print a particular statement (getStatements) for a particular method (get)
-        //System.out.println(methodStatements.get(2).getStatements().get(1));
-
-        for(int i=0;i<methodStatements.size();i++){
-            System.out.println("METHOD NAME: " + methodNames.get(i));
-            System.out.println("STATEMENTS: " + methodStatements.get(i).getStatements());
-            System.out.println("***********************************************");
-        }
     }
 
-    //First Visitor: print all Method Name of my class example "Car"
-    private static class MethodNamePrinter extends VoidVisitorAdapter<Void>{
-        
-        //We want to override the visit method of class VoidVisitorAdapter, for the type we are interested in (MethodDeclaration) 
+    private static class MethodBlockStmt extends VoidVisitorAdapter<List<BlockStmt>>{
         @Override
-        public void visit(MethodDeclaration md, Void arg){
-            super.visit(md, arg);
-            System.out.println("Method Name: " + md.getName());
+        public void visit(BlockStmt md, List<BlockStmt> collector){
+            collector.add(md);
         }
     }
-
-    //Second Visitor: we want to collect in a list all Method name of our class example "Car"
-    private static class MethodNameCollector extends VoidVisitorAdapter<List<String>>{
-
-        @Override
-        public void visit(MethodDeclaration md, List<String> collector) {
-            super.visit(md, collector);
-            collector.add(md.getNameAsString());
-        }
-    }
-
-    //Third Visitor: we want to collect all statements for all method in a List
-    private static class MethodStatementsCollector extends VoidVisitorAdapter<List<BlockStmt>>{
-        @Override
-        public void visit(MethodDeclaration md, List<BlockStmt> collector){
-            super.visit(md, collector);
-            collector.add(md.getBody().get());
-        }
-    }
+    
 }
