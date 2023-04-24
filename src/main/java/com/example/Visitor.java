459dcc16f0eca7ee53ac745c59290f0f2cd2bd1c
Luca Strano <stranoluca469955@gmail.com>
2023-04-24 13:38:36 +0200

    feat: add new visitor MethodName

--- src/main/java/com/example/Visitor.java
+++ src/main/java/com/example/Visitor.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.ast.visitor.VoidVisitor;
 import com.github.javaparser.ast.stmt.BlockStmt;
@@ -17,24 +18,40 @@ public class Visitor{
     public static void main(String[] args) throws Exception{
         
         CompilationUnit cu = StaticJavaParser.parse(new FileInputStream(FILE_PATH));
+        
+        //Visitatore dei nomi di ogni metodo
+        List<String> methodNameList = new ArrayList<>();
+        VoidVisitor<List<String>> methodNameVisitor = new MethodName();
+        methodNameVisitor.visit(cu, methodNameList);
 
-        List<BlockStmt> methodBlocks = new ArrayList<>();
-        VoidVisitor<List<BlockStmt>> methodBlockStmt = new MethodBlockStmt();
-        methodBlockStmt.visit(cu, methodBlocks);
+        //Visitatore degli statements di ogni metodo
+        List<BlockStmt> methodBlocksList = new ArrayList<>();
+        VoidVisitor<List<BlockStmt>> methodBlockStmtVisitor = new MethodBlockStmt();
+        methodBlockStmtVisitor.visit(cu, methodBlocksList);
 
-        for(int i=0;i<methodBlocks.size();i++){ //for each block i ...
-            for(int j=0;j<methodBlocks.get(i).getStatements().size();j++){ //... for each statement j of block i
-                System.out.println(j + ". " + methodBlocks.get(i).getStatements().get(j));
+        //print..
+        //NOTA: cominciamo ad ispezionare la lista methodBlocksList dall'indice 1, perchè l'elemento in posizione 0 conterrà il BlockStmt relativo al costruttore (che a noi non interessa)
+        for(int i=1;i<methodBlocksList.size();i++){ //.. for each block i ...
+            System.out.println("METHOD NAME: " + methodNameList.get(i-1));
+            for(int j=0;j<methodBlocksList.get(i).getStatements().size();j++){ //... for each statement j of block i
+                System.out.println(j + ". " + methodBlocksList.get(i).getStatements().get(j));
             }
             System.out.println("\n");
         }
         
     }
 
+    private static class MethodName extends VoidVisitorAdapter<List<String>>{
+        @Override
+        public void visit(MethodDeclaration md, List<String> collector){
+            collector.add(md.getNameAsString());
+        }
+    }
+
     private static class MethodBlockStmt extends VoidVisitorAdapter<List<BlockStmt>>{
         @Override
-        public void visit(BlockStmt md, List<BlockStmt> collector){
-            collector.add(md);
+        public void visit(BlockStmt bs, List<BlockStmt> collector){
+            collector.add(bs);
         }
     }
     
